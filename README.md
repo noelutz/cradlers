1b02ede8304662687c497279aaf70e69542419e9
Noé Lutz <noe.lutz@gmail.com>
2023-12-16 17:09:41 +0100

    Initial commit of the cradlers app.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cradlers
+
+Fetches the list of all Cradlers from the Cradle website and generates
+an HTML page in the form of flash cards that can be used to learn
+the names of all Cradlers.
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import functions_framework
+import html
+import io
+import typing
+import urllib.request
+
+from bs4 import BeautifulSoup
+from typing import Generator
+
+
+# Fetches the team members from the cradle.bio website.
+# Returns a generator of tuples containing the name, role and image url
+# (in that order).
+def fetchTeamMembers() -> Generator[str | None, str | None, str | None]:
+    with urllib.request.urlopen("http://cradle.bio/team") as response:
+        soup = BeautifulSoup(response.read(), "html.parser")
+        for div in soup.find_all("div", class_="framer-szp9rx-container"):
+            yield div.find("h2").string, div.find("p").string, div.find("img")["src"]
+
+
+# Generates the cradlers HTML file and returns its content.
+def generateCradlers(out: typing.TextIO):
+    card_template: str = open("templates/card_tmpl.html",
+                              encoding="utf-8").read()
+    page_header: str = open("templates/page_header.html",
+                            encoding="utf-8").read()
+    page_footer: str = open("templates/page_footer.html",
+                            encoding="utf-8").read()
+
+    out.write(page_header)
+    for name, role, img in fetchTeamMembers():
+        out.write(
+            card_template.format(html.escape(img),
+                                 html.escape(name),
+                                 html.escape(role))
+        )
+    out.write(page_footer)
+
+
+# If the script is run directly, we output the HTML to cradlers.html.
+if __name__ == "__main__":
+    with open("cradlers.html", "w", encoding="utf-8") as f:
+        generateCradlers(f)
+    exit(1)
+
+
+# If the script is run as a Cloud Function, we output the HTML to the response.
+@functions_framework.http
+def get(request):
+    response = io.StringIO()
+    generateCradlers(response)
+    return response.getvalue()
