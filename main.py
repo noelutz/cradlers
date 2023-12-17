d3393d7e6453a015c97766ebd0cdeffe9293d8c4
Noé Lutz <noe.lutz@gmail.com>
2023-12-17 20:40:02 +0100

    clean up code a bit.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+"""Generates a flip card HTML page to learn Cradlers' names.
+
+Cradle is growing fast. It's hard to keep track of all the new faces.
+This script fetches the list of Cradlers from the website and generates a
+simple flip card HTML file that can be used to learn people's names.
+
+The script can either be invoked locally to generate an HTML file that can
+be served statically, or it can be deployed as a Cloud Function that will
+generate the HTML on demand by fetching the list of Cradlers from the website.
+
+Any bugs should be reported to noe@cradle.bio.
+"""
+
 import functions_framework
 import html
 import io
@@ -9,32 +22,35 @@ from bs4 import BeautifulSoup
 from typing import Generator
 
 
-# Fetches the team members from the cradle.bio website.
-# Returns a generator of tuples containing the name, role and image url
-# (in that order).
-def fetchTeamMembers() -> Generator[str | None, str | None, str | None]:
+def fetch_team_members() -> Generator[str | None, str | None, str | None]:
+    """Fetches the team members from the cradle.bio website.
+
+    Yields:
+      A set of duples containing the name, role and image url (in that order).
+    """
     with urllib.request.urlopen("http://cradle.bio/team") as response:
         soup = BeautifulSoup(response.read(), "html.parser")
-        parent_div = soup.find('div', attrs={'name': 'team grid'})
-        for div in parent_div.find_all("div", attrs={"class": re.compile("framer-.*-container")}):
+        parent_div = soup.find("div", attrs={"name": "team grid"})
+        for div in parent_div.find_all(
+            "div", attrs={"class": re.compile("framer-.*-container")}
+        ):
             yield div.find("h2").string, div.find("p").string, div.find("img")["src"]
 
 
-# Generates the cradlers HTML file and returns its content.
-def generateCradlers(out: typing.TextIO):
-    card_template: str = open("templates/card_tmpl.html",
-                              encoding="utf-8").read()
-    page_header: str = open("templates/page_header.html",
-                            encoding="utf-8").read()
-    page_footer: str = open("templates/page_footer.html",
-                            encoding="utf-8").read()
+def generate_cradlers(out: typing.TextIO):
+    """Generates the cradlers HTML file and writes its content to out.
+
+    Paremeters:
+        out: The file to write the HTML to.
+    """
+    card_template: str = open("templates/card_tmpl.html", encoding="utf-8").read()
+    page_header: str = open("templates/page_header.html", encoding="utf-8").read()
+    page_footer: str = open("templates/page_footer.html", encoding="utf-8").read()
 
     out.write(page_header)
-    for name, role, img in fetchTeamMembers():
+    for name, role, img in fetch_team_members():
         out.write(
-            card_template.format(html.escape(img),
-                                 html.escape(name),
-                                 html.escape(role))
+            card_template.format(html.escape(img), html.escape(name), html.escape(role))
         )
     out.write(page_footer)
 
@@ -42,13 +58,17 @@ def generateCradlers(out: typing.TextIO):
 # If the script is run directly, we output the HTML to cradlers.html.
 if __name__ == "__main__":
     with open("cradlers.html", "w", encoding="utf-8") as f:
-        generateCradlers(f)
+        generate_cradlers(f)
     exit(1)
 
 
-# If the script is run as a Cloud Function, we output the HTML to the response.
 @functions_framework.http
 def get(request):
+    """The Cloud Function entry point. Generates the HTML and returns it.
+
+    Parameters:
+        request: The HTTP request. Not really used.
+    """
     response = io.StringIO()
-    generateCradlers(response)
+    generate_cradlers(response)
     return response.getvalue()
